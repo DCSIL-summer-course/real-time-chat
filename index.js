a41dee33cbf8d85c4fb15d2c6a959723af54b079
Liam Kaufman <liam.kaufman@gmail.com>
2015-06-15 01:49:35 +0000

    can list connected users

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var _ = require('underscore');
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
+});
+
+var users = {};
+var sockets = {}
+
+function broadcastUsers(){
+  _.each(sockets, function(socket){
+    socket.emit('users', {
+      users : Object.keys(users)
+    });
+  });
+}
+
+io.on('connection', function(socket){
+  var name;
+  socket.on('join', function(msg){
+    name = msg.name;
+    users[name] = name;
+    sockets[socket.id] = socket;
+    broadcastUsers();
+  });
+
+  socket.on('disconnect', function(){
+    delete users[name];
+    delete sockets[socket.id];
+    broadcastUsers();
+  });
 });
 
 http.listen(3000, function(){
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,50 @@
-var app = angular.module('realTimeChat', []);
+var app = angular.module('realTimeChat', ['btford.socket-io']);
 
+app.factory('socket', function(socketFactory){
+  return socketFactory();
+});
 
-app.controller('ChatCtrl', function($scope){
-  $scope.time = new Date();
+app.factory('chat', function(socket, $rootScope){
+  var factory = {};
+  factory.users = [];
+
+  factory.getUsersName = function(){
+    factory.name = window.localStorage.getItem('name');
+
+    if(!factory.name){
+      factory.name = window.prompt("Please Enter Your Name: ");
+      window.localStorage.setItem('name', factory.name);
+    }
+
+    return factory.name;
+  };
+
+  factory.joinRoom = function(){
+    socket.emit('join', {
+      name : factory.name
+    });
+  };
+
+  factory.initialize = function(){
+    socket.on('users', function(msg){
+      factory.users = msg.users;
+    });
+
+    factory.getUsersName();
+    factory.joinRoom();
+
+    return factory.name;
+  };
+
+  return factory;
+});
+
+app.controller('ChatCtrl', function($scope, chat){
+  $scope.usersName = chat.initialize();
+  
+  $scope.$watch(function(){
+    return chat.users;
+  }, function(users){
+    $scope.users = chat.users;
+  });
 });
